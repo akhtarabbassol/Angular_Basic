43490295638217a8749c23cc2889746b2325f41a
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2026-08-18 12:54:15 +0500

    Controle Flow added successfully

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,20 @@
-import { Component, computed, signal } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
-   selector:'App' ,
-   templateUrl:'app.html' ,
+   selector:'App ',
+   templateUrl:'app.html',
    styleUrl :'app.css'
 })
-export class myApp {
-   price = signal(20)
-   Quentity = signal(5)
-   total = computed(()=>(this.price()*this.Quentity()))
-   increase(){
-      this.Quentity.update(e=>e+1)
-   }
-   decrease(){
-      this.Quentity.update(e=>e-1)
-   }
+
+export class myApp{
+     User = signal({
+      Name : 'Ali' ,
+      age : 12 
+     })
+     increaseAge(){
+      this.User.update(e=>({
+         ...e ,
+         age : e.age+1
+      }))
+     }
 }
