e25f917db46b031ffbbe96092303fb46f0aa4723
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2026-08-19 21:59:22 +0500

    Linked Signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,44 +1,67 @@
 import { Component, linkedSignal, signal } from "@angular/core";
 
+interface UserInfo{
+    id:number ,
+    Name : string ,
+    Desination:string
+}
+
 @Component({
-    selector:'App' ,
-    templateUrl:'app.html' ,
+    selector :'App',
+    templateUrl:'app.html',
     styleUrl:'app.css'
 })
 
 export class myApp{
-    UserInfo = signal([
+    Users1 = signal<UserInfo[]>([
         {
-            Id : 1 ,
+            id : 1 ,
             Name : "Ali" ,
-            Pmethod : 'JazzCash'
+            Desination : 'Student'
         },
         {
-            Id : 2 ,
-            Name : "Farhat" ,
-            Pmethod : 'EasyPaisa'
+            id : 1 ,
+            Name : "Wafdar" ,
+            Desination : 'Developer'
         },
         {
-            Id : 3,
-            Name : "Waqar" ,
-            Pmethod : 'Bank'
-        },
-        {
-            Id : 4 ,
-            Name : "Aoun" ,
-            Pmethod : 'RasatId'
+            id : 1 ,
+            Name : "Ghayur" ,
+            Desination : 'Developer'
         },
     ])
+    Users2 = signal<UserInfo[]>([
+                {
+            id : 1 ,
+            Name : "Farhat" ,
+            Desination : 'Developer'
+        },
+        {
+            id : 2,
+            Name : "Zarak" ,
+            Desination : 'SEC Expert'
+        },
+        {
+            id : 3 ,
+            Name : "Tasawar" ,
+            Desination : 'Developer'
+        },
+    ])
+   SelectSec = signal(1)
 
-    ShowUser = linkedSignal(()=>this.UserInfo()[0])
-    currentIndex = signal(0)
-    ShowNext(){
-     this.currentIndex.update(e=>{
-       let nextIndex  = (e+1) % this.UserInfo().length ;
-       this.ShowUser.set(this.UserInfo()[nextIndex])
-       return nextIndex ; 
-    })
+   SelectUser = linkedSignal({
+    source:()=>this.SelectSec() ,
+    computation:(sec)=>{
+        if(sec===1){
+            return this.Users1()[0]
+        }
+        else{
+            return this.Users2()[2]
+        }
     }
-
+   })
+   ChangeSec(){
+    this.SelectSec.update(e=>e===1?2:1)
+   }
 
 }
