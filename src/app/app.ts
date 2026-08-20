40263eed26092a408fa4f21afa14a5d439d8c517
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2026-08-20 17:42:22 +0500

    ,

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,67 +1,139 @@
-import { Component, linkedSignal, signal } from "@angular/core";
+import {
+  Component,
+  effect,
+  resource,
+  signal
+} from "@angular/core";
 
-interface UserInfo{
-    id:number ,
-    Name : string ,
-    Desination:string
+interface UserInfo {
+  id: number;
+  name: string;
+  email: string;
+  address: {
+    city: string;
+  };
 }
 
 @Component({
-    selector :'App',
-    templateUrl:'app.html',
-    styleUrl:'app.css'
+  selector: 'App',
+  templateUrl: 'app.html',
+  styleUrl: 'app.css'
 })
+export class myApp {
 
-export class myApp{
-    Users1 = signal<UserInfo[]>([
-        {
-            id : 1 ,
-            Name : "Ali" ,
-            Desination : 'Student'
-        },
-        {
-            id : 1 ,
-            Name : "Wafdar" ,
-            Desination : 'Developer'
-        },
-        {
-            id : 1 ,
-            Name : "Ghayur" ,
-            Desination : 'Developer'
-        },
-    ])
-    Users2 = signal<UserInfo[]>([
-                {
-            id : 1 ,
-            Name : "Farhat" ,
-            Desination : 'Developer'
-        },
-        {
-            id : 2,
-            Name : "Zarak" ,
-            Desination : 'SEC Expert'
-        },
-        {
-            id : 3 ,
-            Name : "Tasawar" ,
-            Desination : 'Developer'
-        },
-    ])
-   SelectSec = signal(1)
+  Userid = signal(1);
 
-   SelectUser = linkedSignal({
-    source:()=>this.SelectSec() ,
-    computation:(sec)=>{
-        if(sec===1){
-            return this.Users1()[0]
-        }
-        else{
-            return this.Users2()[2]
-        }
+  User = resource<UserInfo[], unknown>({
+    loader: async () => {
+
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users`
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to Fetch Data");
+      }
+
+      return response.json();
     }
-   })
-   ChangeSec(){
-    this.SelectSec.update(e=>e===1?2:1)
-   }
+  });
+
+  Users = signal<UserInfo[]>([]);
+
+  // Controls edit box
+  showEditBox = signal(false);
+
+  // Selected user ID
+  selectedUserId = signal<number | null>(null);
+
+  // Form values
+  editName = signal("");
+  editEmail = signal("");
+  editCity = signal("");
+
+  constructor() {
+
+    effect(() => {
+
+      const data = this.User.value();
+
+      if (data) {
+        this.Users.set(data);
+      }
+
+    });
+
+  }
+
+  // Open update box
+  UpdateUser(user: UserInfo) {
+
+    this.selectedUserId.set(user.id);
+
+    this.editName.set(user.name);
+    this.editEmail.set(user.email);
+    this.editCity.set(user.address.city);
+
+    this.showEditBox.set(true);
+  }
+
+  // Save updated user
+  SaveUser() {
+
+    const id = this.selectedUserId();
+
+    if (id === null) {
+      return;
+    }
+
+    this.Users.update(users =>
+      users.map(user => {
+
+        if (user.id === id) {
+
+          return {
+            ...user,
+
+            name: this.editName(),
+
+            email: this.editEmail(),
+
+            address: {
+              ...user.address,
+              city: this.editCity()
+            }
+
+          };
+
+        }
+
+        return user;
+
+      })
+    );
+
+    // Close edit box
+    this.showEditBox.set(false);
+
+    // Clear selected user
+    this.selectedUserId.set(null);
+  }
+
+  // Close update box
+  CancelUpdate() {
+
+    this.showEditBox.set(false);
+
+    this.selectedUserId.set(null);
+  }
+
+  // Delete user
+  DelUser(id: number) {
+
+    this.Users.update(users =>
+      users.filter(user => user.id !== id)
+    );
+
+  }
 
 }
